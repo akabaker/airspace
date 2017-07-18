090d10bfe25a5c2b5e0bc10319462f7157e9abd9
Luke Baker <luke.baker@veteransunited.com>
2017-07-18 07:55:15 -0500

    Just mussin'

--- src/app/nav/navbar/navbar.component.ts
+++ src/app/nav/navbar/navbar.component.ts
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  title: string;
 
-  constructor() { }
+  constructor() {
+    this.title = 'Airspace';
+  }
 
   ngOnInit() {
   }
