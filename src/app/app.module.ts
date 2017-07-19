f322d0f0597651ceba57fde252270abd7d742f1d
Luke Baker <luke.baker@veteransunited.com>
2017-07-19 08:00:24 -0500

    Had to move navbar stuff around

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './nav/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
   ],
   imports: [
     BrowserModule
